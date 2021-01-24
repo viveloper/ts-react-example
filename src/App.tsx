832e8aa6bde2a4ad0011b35c66a5727e75b4eb53
viveloper <ucsik1234@gmail.com>
2021-01-25 07:00:56 +0900

    added MyForm Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
-import Counter from './Counter';
+import MyForm from './MyForm';
 
 function App() {
-  return <Counter />;
+  const onSubmit = (form: { name: string; description: string }) => {
+    console.log(form);
+  };
+  return <MyForm onSubmit={onSubmit} />;
 }
 
 export default App;
